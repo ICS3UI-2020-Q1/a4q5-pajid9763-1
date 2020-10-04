144a13c6439705423f3227b65016607e6b9e5831
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:46:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that prints the hailstone sequence
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  // ask the user for a positive integer
+  System.out.println("Please enter a positive integer to see the hailstone sequence.");
+  int n = input.nextInt();
+  System.out.println("Hailstone sequence is the following:");
+
+  // number has to be greater than 1 for while loop to perform
+  while(n > 1){
     
+    // if number is divisible by 2 
+    if (n % 2 == 0)
+      n /= 2;
+    
+    // if number is not divisible by 2
+    else n = (n * 3) + 1;
+    System.out.println(n);
+  }
+
+
+
   }
 }
